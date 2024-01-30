2b8bac804fd3fa37816d69edae8adf249dcb619e
Amir Khalife <eng.amir.bu@gmail.com>
2024-01-30 23:28:44 +0330

    request.headers is more standard than request.META

--- financipy/utils/ip.py
+++ financipy/utils/ip.py
@@ -2,9 +2,9 @@ from django.http import HttpRequest
 
 
 def get_client_ip(request: HttpRequest) -> str:
-    x_forwarded_for = request.headers.get("x-forwarded-for")
+    x_forwarded_for = request.headers.get("X-Forwarded-For")
     if x_forwarded_for:
         ip = x_forwarded_for.split(",")[0]
     else:
-        ip = request.META.get("REMOTE_ADDR")
+        ip = request.headers.get("Remote-Addr")
     return ip
